c75853242b7251e9f06326502a2289846992117a
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2019-02-12 16:10:00 +0900

    Add safari and permission action

--- Example/Source/MenuViewController.swift
+++ Example/Source/MenuViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import StoreKit
+import SafariServices
+import LocalAuthentication
 
 class MenuViewController: UITableViewController, SKStoreProductViewControllerDelegate {
     
@@ -31,6 +33,20 @@ class MenuViewController: UITableViewController, SKStoreProductViewControllerDel
             }
         }
         present(viewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showSafariTapped(_ sender: Any) {
+        let url = URL(string: "https://google.com")!
+        let viewControlller = SFSafariViewController(url: url)
+        present(viewControlller, animated: true, completion: nil)
+    }
+    
+    @IBAction func showPermissionTapped(_ sender: Any) {
+        let context = LAContext()
+        
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Test") { (success, error) in
+            
+        }
     }
     
     // MARK: - SKStoreProductViewController Delegate
